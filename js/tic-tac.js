88b7e51cca0281ac0fc8f8d5039e7f542a77ec7c
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2015-09-20 13:25:50 -0400

    changed js to track win combinations, fixed tie, fixed double clicking tiles

--- js/tic-tac.js
+++ js/tic-tac.js
@@ -1,138 +1,147 @@
-$(document).ready(function(){
+//variable for function that runs game
 
-  // set variable to track turns
+var tictactoe = (function() {
 
-    var turnCount=0;
+  // set variables to track turns, define user symbols, current player, and board position status
 
-  // display who's turn it is
+    var turnCount=0,
+        X = 'X',
+        O = 'O',
+        currentPlayer = X,
+        open = '',
+        board = [open, open, open, open, open, open, open, open, open, open];
 
-    var displayCurrentPlayer = function(currentPlayer) {
-        $('.currentPlayer').html(currentPlayer);
+  // display game status
+
+    var displayMessage = function(message) {
+        $('.message').html(message);
       };
 
-  // display player's tile symbol on spot they clicked, even turns X, odds O
-  // check if either player has won
+  //tenary operator to toggle activeUser between O and X, displays current player in HTML message field
 
+    var switchPlayer = function() {
+        currentPlayer = ( currentPlayer === X ) ? O : X;
+        displayMessage( 'Current Player: ' + currentPlayer );
+  };
 
-      $('#board').find('td').on('click', function(){
+// will only allow for click events on open board spaces, displays HTML message otherwise
 
-        //check for tie
-
-          if (turnCount === 9) {
-              alert("Game over! It's a tie!");
-              $(this).text('X');
-              $('#newGame').removeClass('invisible');
-              console.log(turnCount);
-          } else {
-
-        //check if turn count is an even integer - if so, it's Player X's turn
-    
-            if (turnCount % 2 === 0){
-              displayCurrentPlayer( 'Current Player: O');
-              $(this).text('X'); 
-              checkForWin('X');
-              console.log(turnCount);
-            } else {
-
-        //otherwise - it's Player Os turn
-
-              displayCurrentPlayer( 'Current Player: X');
-              $(this).text('O');
-              checkForWin('O');
-              console.log(turnCount);
-            }
-            turnCount++;
-              };
-      });
-
- console.log(turnCount);
-
-     //if number of turns is greater than spaces = it's a tie
-     //if click the same spot twice, no change
-
-    // Function to check each row, column and diagonal for 3 consecutive Xs or Os, if so - display "new game button" 
-     
-    
-    function checkForWin(player){
-       //top row check
-        if ($('#board').find('#1').text() !== ''){
-            if ($('#board').find('#1').text() == $('#board').find('#2').text()) {
-                if ($('#board').find('#1').text() == $('#3').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        //left column check
-            if ($('#board').find('#1').text() == $('#board').find('#4').text()) {
-                if ($('#board').find('#1').text() == $('#7').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        //left diagonal check
-            if ($('#board').find('#1').text() == $('#board').find('#5').text()) {
-                if ($('#board').find('#1').text() == $('#9').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }    
-        }
-        
-       //middle column check
-        if ($('#board').find('#2').text() !== ''){
-            if ($('#board').find('#2').text() == $('#board').find('#5').text()) {
-                if ($('#board').find('#2').text() == $('#8').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        }
-        
-        //right column check
-        if ($('#board').find('#3').text() !== ''){
-            if ($('#board').find('#3').text() == $('#board').find('#6').text()) {
-                if ($('#board').find('#3').text() == $('#9').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-            //right diag check
-            if ($('#board').find('#3').text() == $('#board').find('#5').text()) {
-                if ($('#board').find('#3').text() == $('#7').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        }
-        
-        //middle row check
-        
-        if ($('#board').find('#4').text() !== ''){
-            if ($('#board').find('#4').text() == $('#board').find('#5').text()) {
-                if ($('#board').find('#4').text() == $('#6').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        }
-        
-        //bottom row check
-        if ($('#board').find('#7').text() !== ''){
-            if ($('#board').find('#7').text() == $('#board').find('#8').text()) {
-                if ($('#board').find('#7').text() == $('#9').text()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        }     
+  var isValidMove = function( index ) {
+    if ( board[ index ] === open ) {
+      return true;
+    } else {
+      displayMessage( 'That space is already taken!' );
+      return false;
     }
+  };
 
-// Play again, reset turn count, reset player turn to player X
+  //alters location game symbol is placed based on user input, hides "play again button" 
+  //increases turn count with each move
 
-    $('#newGame').on('click',function(){
-        $('.square').text('');
-        turnCount = 0;
-        displayCurrentPlayer( 'Current Player: X');
+    var makeMove = function( $square, index ) {
+      board[ index ] = currentPlayer;
+      $square.html( currentPlayer );
+      $( '.play-again' ).hide();
+      turnCount++;
+  };
+
+
+//jQuery each loop returns compares rows, heights, diagonal to winning combinations
+//if user wins, winIndex is set to -1
+//if user does not win returns false, winIndex is reset and game continues/
+
+  var userWins = function() {
+    var winCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+                            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ],
+        winIndex = -1;
+    $.each( winCombinations, function( index, winCombination ) {
+      if( allEqual( winCombination ) ){
+        winIndex = index;
+        return false;
+      }
     });
 
+    //if user wins, returns which winCombination led to them winning
+
+    if( winIndex !== -1 ) {
+        return winCombinations[ winIndex ];
+
+    //stops game in event of a tie
+
+    } else if ( turnCount === 9 ) {
+        return true; 
+    } else {
+        return false;
+    }
+  };
+
+  //tracks instances of 3 consecutive symbols (X, O, open by indexes)
+
+    var allEqual = function( indexes ) {
+    return ( board[ indexes[0] ] === board[ indexes[1] ] ) &&
+           ( board[ indexes[0] ] === board[ indexes[2] ] ) &&
+           ( board[ indexes[0] ] !== open );
+  };
+
+
+//After gameOver, function has run will return message with which current Player has Won , or Tie. 
+//If a user won, will highlight in green winning combination of moves
+
+    var gameOver = function( endFormation ) {
+    var resultMessage;
+
+    if( $.isArray(endFormation) ){
+      resultMessage = 'Game Over.  Player ' + currentPlayer + ' Wins';
+      showWinFormation( endFormation );
+    } else {
+      resultMessage = 'Game Over.  Draw Game';
+    }
+    $( '.message' ).addClass( 'end-message' );
+    displayMessage( resultMessage );
+
+
+
+    // Turn off gameboard click listener/ show play again button
+
+    $('.gameboard').off('click');
+    $( '.play-again' ).show().on( 'click', function() {
+        location.reload();
+    });
+
+  };
+
+  // Add a class to highlight the winning squares green to display to users
+  // jquery each loop to add 'winning-square' class to each square part of winning combination
+
+  var showWinFormation = function( formation ) {
+    $.each( formation, function( index, winPosition ) {
+      $( '.square' ).eq( winPosition ).addClass(' winning-square ');
+    });
+  };
+
+  // Main game functionality. Adds id to squares that are clicked, verifies movie is validate
+  // if a winning Formation appears, user wins , winning formation is displayed, and player is switched
+
+  var play = function( $square ) {
+    var index = +$square.attr( 'id' );
+
+    if( isValidMove( index ) ){
+      makeMove( $square, index );
+      var winningFormation = userWins();
+
+      ( winningFormation ) ? gameOver( winningFormation ) : switchPlayer();
+    }
+  };
+
+//return play from tictac toe function
+  return { play: play };
+
+})();
+
+//loads game into the document window, calls tictactoe.play when a square on game board is clicked
+
+$( document ).ready( function() {
+  $( '.gameboard' ).on( 'click', '.square', function() {
+    tictactoe.play( $(this) );
+  });
 });
